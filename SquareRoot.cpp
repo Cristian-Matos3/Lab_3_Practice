c4fdf32c4f7d0ef116c76cd695495f8d123bbdef
Cristian-Matos3 <cristian.matos3@upr.edu>
2022-02-17 21:04:10 -0400

    CommitSquareRootFile

---
+++ SquareRoot.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void SquareRoot(double num)
+{
+    double result = sqrt(num);
+    cout << result << endl;
+}
+
+int main()
+{
+    SquareRoot(4);
+    return 0;
+}
